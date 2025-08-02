defabbbd0d0a2e2a04750be65b753ee2d766afb6
Rajeev <rajeevbhorgade9589@gmail.com>
2025-08-02 22:06:16 +0530

    Update the middleware added userId as optional user tracking.

--- core/tracemiddleware.js
+++ core/tracemiddleware.js
@@ -1,15 +1,15 @@
 import { logRequest, checkRateLimit } from './helper.js';
-
 const traceMiddleware = async (req, res, next) => {
   try {
     const apiKey = req.headers['x-api-key'];
     const appId = req.headers['x-app-id'];
+    const userId = req.headers['x-user-id'] || ''; // user tracking
 
     if (!apiKey || !appId) {
       return res.status(403).json({ msg: 'Missing API credentials' });
     }
 
-    const blocked = await checkRateLimit(appId, req.ip);
+    const blocked = await checkRateLimit(appId, req.ip, userId);
     if (blocked) {
       return res.status(429).json({ msg: 'You are being rate limited or blocked' });
     }
